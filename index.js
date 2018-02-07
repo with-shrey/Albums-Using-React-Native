10094aa72fd12ae39f3e9fe8b16afff12addb744
Shrey Gupta <shrey.gupta1nov@gmail.com>
2018-02-08 02:48:17 +0530

    index

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+// AppRegistry.registerComponent('albums', () => App);
 
+//Import a library to help create a registerComponent
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+//Create a registerComponent
+// const App = () => {
+//   return (
+//     <Text>Some text</Text>
+//   );
+// };
+const App = () => (
+
+    <Text>Shrey text</Text>
+  );
+
+
+//Render it to the device
 AppRegistry.registerComponent('albums', () => App);
